da59038662bf5560af0a70a5604ac5594bed70e6
SayuriYuto <dhuriamey02@gmail.com>
2022-08-13 22:00:49 +0530

    objects in JS

--- index.js
+++ index.js
@@ -18,4 +18,20 @@ let selectedColor = null; // to clear the value
 // JavaScript is a Dynamic Language
 // type of variable can change in runtime
 name = 89;
-console.log(typeof name); 
+console.log(typeof name);
+console.log(typeof selectedColor);
+
+// Objects in JS
+let person = {
+    firstName :'Amey',
+    age : 20
+};
+
+console.log(person.age);
+console.log(person.firstName);
+person.firstName = 'newNAme';
+console.log(person.firstName);
+
+let selection = 'firstName'
+person[selection] = 'this is also a name';
+console.log(person);
