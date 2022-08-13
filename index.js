27c4a8f03e2050d0602c0f8297f3fe0e3e9df484
SayuriYuto <dhuriamey02@gmail.com>
2022-08-13 21:44:26 +0530

    main

---
+++ index.js
@@ -0,0 +1,15 @@
+// This is a comment
+console.log('This is the print statement')
+
+let lulli = 'Amey';
+console.log(lulli)
+
+const interest = 0.3; // constant value
+// interest = 1;
+console.log(interest);
+
+// Variable types
+let name = 'Name';
+let age = 59;
+let isApproved = true;
+
