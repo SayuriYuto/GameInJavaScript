a469fc2c102feea8996c66f4d401d36705f23d72
SayuriYuto <dhuriamey02@gmail.com>
2022-08-15 16:30:53 +0530

    Made CanvasContext

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d') // c is the context
+
 canvas.width = 1024
 canvas.height = 576
-console.log(canvas)
+
+c.fillStyle = 'white'
+c.fillRect(0,0,canvas.width,canvas.height)
