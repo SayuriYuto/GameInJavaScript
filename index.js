0cb27dfcf9af2d4ba4349e719fe9d3a7a99340ec
SayuriYuto <dhuriamey02@gmail.com>
2022-08-15 16:55:33 +0530

    IntroMap Added into canvas

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d') // c is the context
+const c = canvas.getContext('2d') // c is the context for canvas
 
 canvas.width = 1024
 canvas.height = 576
 
 c.fillStyle = 'white'
 c.fillRect(0,0,canvas.width,canvas.height)
+
+// create html image object to add images from Js
+const image = new Image();
+image.src = './Kings and Pigs/Tiledset/IntroMapView.png'
+
+// when image loads then only call the drawImage function
+// image view level set to 300% in Tiled
+image.onload = () => {
+    c.drawImage(image,0,0)
+}
