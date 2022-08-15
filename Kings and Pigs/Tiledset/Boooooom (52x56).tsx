2ce0e4fdc97ff68ca6101d14af8becadc0fad063
SayuriYuto <dhuriamey02@gmail.com>
2022-08-15 16:17:54 +0530

    Changing Course to Making a Game in JS

---
+++ Kings and Pigs/Tiledset/Boooooom (52x56).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Boooooom (52x56)" tilewidth="52" tileheight="56" tilecount="6" columns="6">
+ <image source="../Sprites/09-Bomb/Boooooom (52x56).png" width="312" height="56"/>
+</tileset>
---
+++ Kings and Pigs/Tiledset/Opening (46x56).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Opening (46x56)" tilewidth="46" tileheight="56" tilecount="5" columns="5">
+ <image source="../Sprites/11-Door/Opening (46x56).png" width="230" height="56"/>
+</tileset>
